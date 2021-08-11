a699544a1cef44d60d7937389613c1602e5d992d
Arpita Roy <arpita.roy.workar@gmail.com>
2021-08-11 20:07:27 +0530

    add the isAlive variable

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-let firstCard = 10
-let secondCard = 7
-let sum = firstCard + secondCard + 4
-
-// Write the conditional according to these rules:
-
-// if less than or equal to 20 -> "Do you want to draw a new card? 🙂"
-// else if exactly 21 -> "Wohoo! You've got Blackjack! 🥳"
-// else -> "You're out of the game! 😭"
-
-
+let firstCard = 1
+let secondCard = 11
+let sum = firstCard + secondCard +2;
+let hasBlackJack = false
+// 1. Create a variable called isAlive and assign it to true
+let isAlive = true;
+// 2. Flip its value to false in the appropriate code block 
 if (sum <= 20) {
     console.log("Do you want to draw a new card? 🙂")
 } else if (sum === 21) {
     console.log("Wohoo! You've got Blackjack! 🥳")
+    hasBlackJack = true
 } else {
     console.log("You're out of the game! 😭")
+    isAlive = false;
 }
+
+// 3. Log it out to check that you're doing it right
+console.log(isAlive);
+console.log(hasBlackJack);
