a4b7bb8f71073207e2e3de1635d23e8d233ec7dd
Arpita Roy <arpita.roy.workar@gmail.com>
2021-08-16 22:48:13 +0530

    push a new card to the array

--- index.js
+++ index.js
@@ -31,10 +31,13 @@ function renderGame() {
     messageEl.textContent = message
 }
 
-
 function newCard() {
     let card = 6
     sum += card
+    // Push the card to the cards array
+    cards.push(card)
+    console.log(cards)
     renderGame()
 }
+
 
