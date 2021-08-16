8c8a09f68a235859e03891ade403dd77b4cadfe6
Arpita Roy <arpita.roy.workar@gmail.com>
2021-08-16 22:08:50 +0530

    Adding and removing items from arrays

--- index.js
+++ index.js
@@ -1,29 +1,16 @@
-// 0 indexed
-let featuredPosts = [
-    "Check out my Netflix clone",        // 0 
-    "Here's the code for my project",    // 1
-    "I've just relaunched my portfolio"  // 2
+let cards = [7, 4]
+cards.push(6)
+//console.log(cards)
+
+// Push the newMessage to the messages array, and then log out the array
+
+let messages = [
+    "Hey, how's it going?",        
+    "I'm great, thank you! How about you?",
+    "All good. Been working on my portfolio lately."
 ]
 
-console.log( featuredPosts.length )
+let newMessage = "Same here!"
 
-let experience = ["CEO at Scrimba", "Frontend developer at Xeneta", "People counter for Norstat"]
-
-// Make the following appear in the console:
-// Frontend developer at Xeneta
-// People counter for Norstat
-// CEO at Scrimba
-console.log(experience[1]);
-console.log(experience[2]);
-console.log(experience[0]);
-// Array - ordered list of items - coposite / complex data type
-
-// Create an array that describes yourself. Use the three primitive data types you've learned
-// It should contain your name (string), your age (number), and whether you like pizza (boolean)
-
-let mySelf = [
-    name      = 'Arpita Roy',
-    age       = 25,
-    likePizza = false
-]
-console.log(mySelf);
+messages.push(newMessage);
+console.log(messages);
