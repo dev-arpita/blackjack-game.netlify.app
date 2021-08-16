e82f0f3a861eb00b620ba7146e5490a428d082be
Arpita Roy <arpita.roy.workar@gmail.com>
2021-08-16 20:51:13 +0530

    Arrays with multiple data type

--- index.js
+++ index.js
@@ -1,47 +1,3 @@
-let firstCard = 10;
-let secondCard = 11;
-let cards = [firstCard, secondCard]; // array - ordered list of items
-let sum = firstCard + secondCard ;
-let hasBlackJack = false;
-let isAlive = true;
-let message = " ";
-let messageEl = document.getElementById("message-el");
-//let sumEl = document.getElementById("sum-el");
-let sumEl = document.querySelector("#sum-el");
-// 2. Store the cards paragraph in a variable called cardsEl
-let cardsEl = document.getElementById("cards-el")
-// Create a new function called startGame() that calls renderGame()
-function startGame() {
-    renderGame();
-}
-function renderGame() {
-    // render out firstCard and secondCard
-    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
-    // render out ALL the cards we have
-    sumEl.textContent = " Sum: "+ sum;
-    if (sum <= 20) {
-        message ="Do you want to draw a new card?" ;
-    } else if (sum === 21) {
-        message = "You've got Blackjack!" ;
-        hasBlackJack = true
-    } else {
-        message = "You're out of the game!" ;
-        isAlive = false
-    }
-   messageEl.textContent = message;
-}
-// 2. Create a function newCard() that logs out "Drawing a new card from the deck!"
-function newCard() {
-    console.log("Drawing a new card from the deck!")
-    // 1. Create a card variable, and hard code its value to a number (2-11)
-    let card = 6
-    // 2. Add the new card to the sum variable
-    sum += card
-    // 3. Call startGame()
-    renderGame()
-}
-newCard();
-
 // 0 indexed
 let featuredPosts = [
     "Check out my Netflix clone",        // 0 
@@ -60,3 +16,14 @@ let experience = ["CEO at Scrimba", "Frontend developer at Xeneta", "People coun
 console.log(experience[1]);
 console.log(experience[2]);
 console.log(experience[0]);
+// Array - ordered list of items - coposite / complex data type
+
+// Create an array that describes yourself. Use the three primitive data types you've learned
+// It should contain your name (string), your age (number), and whether you like pizza (boolean)
+
+let mySelf = [
+    name      = 'Arpita Roy',
+    age       = 25,
+    likePizza = false
+]
+console.log(mySelf);
