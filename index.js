a640cb16659c175c82cadd6f5b0e54a9dda2e31f
Arpita Roy <arpita.roy.workar@gmail.com>
2021-08-16 20:38:59 +0530

    Array indexes

--- index.js
+++ index.js
@@ -1,26 +1,62 @@
-// Arrays - ordered lists of items
+let firstCard = 10;
+let secondCard = 11;
+let cards = [firstCard, secondCard]; // array - ordered list of items
+let sum = firstCard + secondCard ;
+let hasBlackJack = false;
+let isAlive = true;
+let message = " ";
+let messageEl = document.getElementById("message-el");
+//let sumEl = document.getElementById("sum-el");
+let sumEl = document.querySelector("#sum-el");
+// 2. Store the cards paragraph in a variable called cardsEl
+let cardsEl = document.getElementById("cards-el")
+// Create a new function called startGame() that calls renderGame()
+function startGame() {
+    renderGame();
+}
+function renderGame() {
+    // render out firstCard and secondCard
+    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
+    // render out ALL the cards we have
+    sumEl.textContent = " Sum: "+ sum;
+    if (sum <= 20) {
+        message ="Do you want to draw a new card?" ;
+    } else if (sum === 21) {
+        message = "You've got Blackjack!" ;
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!" ;
+        isAlive = false
+    }
+   messageEl.textContent = message;
+}
+// 2. Create a function newCard() that logs out "Drawing a new card from the deck!"
+function newCard() {
+    console.log("Drawing a new card from the deck!")
+    // 1. Create a card variable, and hard code its value to a number (2-11)
+    let card = 6
+    // 2. Add the new card to the sum variable
+    sum += card
+    // 3. Call startGame()
+    renderGame()
+}
+newCard();
 
+// 0 indexed
 let featuredPosts = [
-    "Check out my Netflix clone",
-    "Here's the code for my project",
-    "I've just relaunched my portfolio"
+    "Check out my Netflix clone",        // 0 
+    "Here's the code for my project",    // 1
+    "I've just relaunched my portfolio"  // 2
 ]
 
-// Create an array that lists your i.e. experience, education, licenses, skills or similar
-// The items of the array should be strings
+console.log( featuredPosts.length )
 
 let experience = ["CEO at Scrimba", "Frontend developer at Xeneta", "People counter for Norstat"]
 
-// Arrays - ordered lists of items
-
-let featuredPosts = [
-    "Check out my Netflix clone",
-    "Here's the code for my project",
-    "I've just relaunched my portfolio"
-]
-
-// Create an array that lists your i.e. experience, education, licenses, skills or similar
-// The items of the array should be strings
-
-let experience = ["CEO at Scrimba", "Frontend developer at Xeneta", "People counter for Norstat"]
-
+// Make the following appear in the console:
+// Frontend developer at Xeneta
+// People counter for Norstat
+// CEO at Scrimba
+console.log(experience[1]);
+console.log(experience[2]);
+console.log(experience[0]);
