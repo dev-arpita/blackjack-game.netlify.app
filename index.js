a72977907bd325d5cb4d565427486f63a3c20ec8
Arpita Roy <arpita.roy.workar@gmail.com>
2021-08-17 20:30:22 +0530

    Write your first loop

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-// Count to ten!
+// for (let i = 0; i < 6; i += 1) {
+//     console.log(i)
+// }
 
-// We need to specify...
+// Create a for loop that counts from 10 to 100 in steps of 10
+// Use console.log to log out the numbers
 
-// Where should we START counting?
-// Where is the FINISH line?
-// What's the STEP SIZE we should use?
-//      START         FINISH        STEP SIZE
-
-for (let count = 10; count < 21 ;   count += 1) {
-    console.log(count);
+for (let i = 10; i < 101; i += 10) {
+    console.log(i)
 }
