0437b88415272ad20af4d1a06d868a8d96716031
Arpita Roy <arpita.roy.workar@gmail.com>
2021-08-17 22:36:50 +0530

    Write your first array-based for loop

--- index.js
+++ index.js
@@ -1,21 +1,8 @@
-let messages = [
-    "Hey, how's it going?",        
-    "I'm great, thank you! How about you?",
-    "All good. Been working on my portfolio lately.",
-    "Same here!",
-    "Great to hear",
-    "🙌"
-]
+let cards = [7, 3, 9]
 
-// DRY - Don't repeat yourself
-// console.log(messages[0])
-// console.log(messages[1])
-// console.log(messages[2])
-// console.log(messages[3])
-// 👇 messages.length grab the all messages from top first to bottom last 
-for (let i = 0; i < messages.length; i += 1) {
-    console.log(messages[i])
+// Create a for loop that logs out all the cards in the array
+// Use cards.length to specify how long the loop should run
+
+for (let i = 0; i <cards.length; i ++) {
+   console.log(cards[i]); 
 }
-// for (let i = 0; i < messages[i]; i += 1) {
-//     console.log(messages[i]);
-// }
