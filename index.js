224a815aaafb0dad0b7b56e14ddf486241e17e24
Arpita Roy <arpita.roy.workar@gmail.com>
2021-08-21 23:34:26 +0530

    Completing our dice function

--- index.js
+++ index.js
@@ -56,3 +56,8 @@ let randomNumber = Math.floor(Math.random()*6 ) + 1 ;
 
 console.log(randomNumber);
 // Create a function, rollDice(), that returns a random number between 1 and 6
+function rollDice() {
+    let randomNumber = Math.floor(Math.random()*6 ) + 1 ;
+
+    return randomNumber;    
+}
