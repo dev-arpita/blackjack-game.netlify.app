6cacc4caae06271c6cd40aeda0524a9cc9ce363c
Arpita Roy <arpita.roy.workar@gamil.com>
2022-08-09 16:31:56 +0530

    Apply AND operator to blackjack game

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+
+//logical OR operator:
+// Create two boolean variables, likesDocumentaries and likesStartups
+// Use an OR statement (||) to call recommendMovie() if either of those variables are true
+
+let likesDocumentaries = true
+let likesStartups = true
+
+if (likesDocumentaries === true || likesStartups === true) {
+    recommendMovie()
+}
+
+function recommendMovie() {
+    console.log("Hey, check out this new film we think you will like!")
+}
+
+//Apply AND operator to blackjack game
 let cards = []
 let sum = 0
 let hasBlackJack = false
