9143d0f72ca03a12e62d979cb0608cfa4be5cbd4
Arpita Roy <arpita.roy.workar@gamil.com>
2022-08-09 16:25:07 +0530

    Only trigger newCard()  if you're allowed to

--- index.js
+++ index.js
@@ -1,72 +1,59 @@
-// let hasBlackJack = false
-// let isAlive = true
-// let message = ""
-// let messageEl = document.getElementById("message-el")
-// let sumEl = document.getElementById("sum-el")
-// let cardsEl = document.getElementById("cards-el")
+let cards = []
+let sum = 0
+let hasBlackJack = false
+let isAlive = false
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
 
-// function getRandomCard() {
-//     let  randomNumber = Math.floor(Math.random() * 13 ) + 1
-//    if (randomNumber > 10) {
-//        return 10
-//    }else if (randomNumber === 1) {
-//     return 11
-//    }else {
-//        return randomNumber
-//    }
-// }
-
-
-// function startGame() {
-//     isAlive = true
-//     // Generate two random numbes
-//     let firstCard = getRandomCard()
-//     let secondCard = getRandomCard()
-//     // Re-assign the cards and sum variables so that the game can start
-//     let cards = [firstCard, secondCard]
-//     let sum = firstCard + secondCard
-//     renderGame()
-// }
-
-// function renderGame() {
-//     cardsEl.textContent = "Cards: "
-//     for (let i = 0; i < cards.length; i++) {
-//         cardsEl.textContent += cards[i] + " "
-//     }
-
-//     sumEl.textContent = "Sum: " + sum
-//     if (sum <= 20) {
-//         message = "Do you want to draw a new card?"
-//     } else if (sum === 21) {
-//         message = "You've got Blackjack!"
-//         hasBlackJack = true
-//     } else {
-//         message = "You're out of the game!"
-//         isAlive = false
-//     }
-//     messageEl.textContent = message
-// }
-
-
-// function newCard() {
-    // let card = getRandomCard()
-//     sum += card
-//     cards.push(card)
-//     renderGame()
-// }
-
-
-//logical OR operator:
-// Create two boolean variables, likesDocumentaries and likesStartups
-// Use an OR statement (||) to call recommendMovie() if either of those variables are true
-
-let likesDocumentaries = true
-let likesStartups = true
-
-if (likesDocumentaries === true || likesStartups === true) {
-    recommendMovie()
+function getRandomCard() {
+    let randomNumer = Math.floor( Math.random()*13 ) + 1
+    if (randomNumer > 10) {
+        return 10
+    } else if (randomNumer === 1) {
+        return 11
+    } else {
+        return randomNumer
+    }
 }
 
-function recommendMovie() {
-    console.log("Hey, check out this new film we think you will like!")
+function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    renderGame()
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
+}
+
+
+function newCard() {
+    // Only allow the player to get a new card if she IS alive and does NOT have Blackjack
+    if (isAlive === true && hasBlackJack === false) {
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        renderGame()
+    }
+
 }
