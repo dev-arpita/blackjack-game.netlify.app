7a06c9319fab87f038d1b28366fe9ec4cb51466d
Arpita Roy <arpita.roy.workar@gamil.com>
2022-08-09 17:22:10 +0530

    Intro to Objects

--- index.js
+++ index.js
@@ -1,68 +1,85 @@
 
-let cards = []
-let sum = 0
-let hasBlackJack = false
-let isAlive = false
-let message = ""
-let messageEl = document.getElementById("message-el")
-let sumEl = document.getElementById("sum-el")
-let cardsEl = document.getElementById("cards-el")
+// let cards = []
+// let sum = 0
+// let hasBlackJack = false
+// let isAlive = false
+// let message = ""
+// let messageEl = document.getElementById("message-el")
+// let sumEl = document.getElementById("sum-el")
+// let cardsEl = document.getElementById("cards-el")
 
-// Add player details (with Object data type)
-let player = {
-    name  : Arpita,
-    chips : 145
+// // Add player details (with Object data type)
+// let player = {
+//     name  : Arpita,
+//     chips : 145
+// }
+// let playerEl = document.getElementById("player-el")
+// playerEl.textContent = player.name + ": $" + player.chips;
+
+// function getRandomCard() {
+//     let randomNumer = Math.floor( Math.random()*13 ) + 1
+//     if (randomNumer > 10) {
+//         return 10
+//     } else if (randomNumer === 1) {
+//         return 11
+//     } else {
+//         return randomNumer
+//     }
+// }
+
+// function startGame() {
+//     isAlive = true
+//     let firstCard = getRandomCard()
+//     let secondCard = getRandomCard()
+//     cards = [firstCard, secondCard]
+//     sum = firstCard + secondCard
+//     renderGame()
+// }
+
+// function renderGame() {
+//     cardsEl.textContent = "Cards: "
+//     for (let i = 0; i < cards.length; i++) {
+//         cardsEl.textContent += cards[i] + " "
+//     }
+
+//     sumEl.textContent = "Sum: " + sum
+//     if (sum <= 20) {
+//         message = "Do you want to draw a new card?"
+//     } else if (sum === 21) {
+//         message = "You've got Blackjack!"
+//         hasBlackJack = true
+//     } else {
+//         message = "You're out of the game!"
+//         isAlive = false
+//     }
+//     messageEl.textContent = message
+// }
+
+
+// function newCard() {
+//     // Only allow the player to get a new card if she IS alive and does NOT have Blackjack
+//     if (isAlive === true && hasBlackJack === false) {
+//         let card = getRandomCard()
+//         sum += card
+//         cards.push(card)
+//         renderGame()
+//     }
+
+// }
+
+// Objects - store data in-depth - composite / complex data type
+// key-value pairs
+
+let course = {
+    title: "Learn CSS Grid for free",
+    lessons: 16,
+    creator: "Per Harald Borgen",
+    length: 63,
+    level: 2,
+    isFree: true,
+    tags: ["html", "css"]
 }
-let playerEl = document.getElementById("player-el")
-playerEl.textContent = player.name + ": $" + player.chips;
-
-function getRandomCard() {
-    let randomNumer = Math.floor( Math.random()*13 ) + 1
-    if (randomNumer > 10) {
-        return 10
-    } else if (randomNumer === 1) {
-        return 11
-    } else {
-        return randomNumer
-    }
-}
-
-function startGame() {
-    isAlive = true
-    let firstCard = getRandomCard()
-    let secondCard = getRandomCard()
-    cards = [firstCard, secondCard]
-    sum = firstCard + secondCard
-    renderGame()
-}
-
-function renderGame() {
-    cardsEl.textContent = "Cards: "
-    for (let i = 0; i < cards.length; i++) {
-        cardsEl.textContent += cards[i] + " "
-    }
-
-    sumEl.textContent = "Sum: " + sum
-    if (sum <= 20) {
-        message = "Do you want to draw a new card?"
-    } else if (sum === 21) {
-        message = "You've got Blackjack!"
-        hasBlackJack = true
-    } else {
-        message = "You're out of the game!"
-        isAlive = false
-    }
-    messageEl.textContent = message
-}
 
 
-function newCard() {
-    // Only allow the player to get a new card if she IS alive and does NOT have Blackjack
-    if (isAlive === true && hasBlackJack === false) {
-        let card = getRandomCard()
-        sum += card
-        cards.push(card)
-        renderGame()
-    }
-
-}
+console.log( course.tags ) //.Dot notations
+// console.log( course["tags"] ) //[]bracket notations
