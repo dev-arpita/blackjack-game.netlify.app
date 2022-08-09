59cdaa39c3fbaed4ff94442ce1cca0f029713613
Arpita Roy <arpita.roy.workar@gamil.com>
2022-08-09 17:42:10 +0530

    Create your first object

--- index.js
+++ index.js
@@ -67,19 +67,37 @@
 
 // }
 
-// Objects - store data in-depth - composite / complex data type
-// key-value pairs
 
-let course = {
-    title: "Learn CSS Grid for free",
-    lessons: 16,
-    creator: "Per Harald Borgen",
-    length: 63,
-    level: 2,
-    isFree: true,
-    tags: ["html", "css"]
+// Create an object that represents an airbnb castle listing.
+// It should contain at least one boolean, one string, one number, and one array
+// Log out at least two of the keys using the dot notation
+
+let airbnbCastleListing = {
+    title    : "Live like a king in my castle",
+    price    : 190,
+    perNight : true,
+    facility : [
+        "4 guests" ,
+        "1 bedroom",
+        "2 beds",
+        "private half baths"
+    ]
 }
 
+console.log(airbnbCastleListing .facility)
+console.log(airbnbCastleListing .perNight)
 
-console.log( course.tags ) //.Dot notations
-// console.log( course["tags"] ) //[]bracket notations
+// Create an object that represents an airbnb castle listing.
+// It should contain at least one boolean, one string, one number, and one array
+// Log out at least two of the keys using the dot notation
+
+let castle = {
+    title: "Live like a king in my castle",
+    price: 190,
+    isSuperHost: true,
+    images: ["img/castle1.png", "img/caste2.png"]
+}
+
+console.log(castle.price)
+console.log(castle.isSuperHost)
+
