67aef6c6422b32cc6a345864917bf7059ec84a05
Arpita Roy <arpita.roy.workar@gamil.com>
2022-08-09 17:50:59 +0530

    Use an object to store player data

--- index.js
+++ index.js
@@ -1,103 +1,65 @@
-
-// let cards = []
-// let sum = 0
-// let hasBlackJack = false
-// let isAlive = false
-// let message = ""
-// let messageEl = document.getElementById("message-el")
-// let sumEl = document.getElementById("sum-el")
-// let cardsEl = document.getElementById("cards-el")
-
-// // Add player details (with Object data type)
-// let player = {
-//     name  : Arpita,
-//     chips : 145
-// }
-// let playerEl = document.getElementById("player-el")
-// playerEl.textContent = player.name + ": $" + player.chips;
-
-// function getRandomCard() {
-//     let randomNumer = Math.floor( Math.random()*13 ) + 1
-//     if (randomNumer > 10) {
-//         return 10
-//     } else if (randomNumer === 1) {
-//         return 11
-//     } else {
-//         return randomNumer
-//     }
-// }
-
-// function startGame() {
-//     isAlive = true
-//     let firstCard = getRandomCard()
-//     let secondCard = getRandomCard()
-//     cards = [firstCard, secondCard]
-//     sum = firstCard + secondCard
-//     renderGame()
-// }
-
-// function renderGame() {
-//     cardsEl.textContent = "Cards: "
-//     for (let i = 0; i < cards.length; i++) {
-//         cardsEl.textContent += cards[i] + " "
-//     }
-
-//     sumEl.textContent = "Sum: " + sum
-//     if (sum <= 20) {
-//         message = "Do you want to draw a new card?"
-//     } else if (sum === 21) {
-//         message = "You've got Blackjack!"
-//         hasBlackJack = true
-//     } else {
-//         message = "You're out of the game!"
-//         isAlive = false
-//     }
-//     messageEl.textContent = message
-// }
-
-
-// function newCard() {
-//     // Only allow the player to get a new card if she IS alive and does NOT have Blackjack
-//     if (isAlive === true && hasBlackJack === false) {
-//         let card = getRandomCard()
-//         sum += card
-//         cards.push(card)
-//         renderGame()
-//     }
-
-// }
-
-
-// Create an object that represents an airbnb castle listing.
-// It should contain at least one boolean, one string, one number, and one array
-// Log out at least two of the keys using the dot notation
-
-let airbnbCastleListing = {
-    title    : "Live like a king in my castle",
-    price    : 190,
-    perNight : true,
-    facility : [
-        "4 guests" ,
-        "1 bedroom",
-        "2 beds",
-        "private half baths"
-    ]
+// 2. Create the player object. Give it two keys, name and chips, and set their values
+let player = {
+    name : "Arpita",
+    chips: 145
+}
+let cards = []
+let sum = 0
+let hasBlackJack = false
+let isAlive = false
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+// 3. Grab ahold of the player-el paragraph and store it in a variable called playerEl
+let playerEl = document.getElementById("player-el")
+// 4. Render the player's name and chips in playerEl
+playerEl.textContent = player["name"] + ": $" + player["chips"]
+function getRandomCard() {
+    let randomNumber = Math.floor( Math.random()*13 ) + 1
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
 }
 
-console.log(airbnbCastleListing .facility)
-console.log(airbnbCastleListing .perNight)
-
-// Create an object that represents an airbnb castle listing.
-// It should contain at least one boolean, one string, one number, and one array
-// Log out at least two of the keys using the dot notation
-
-let castle = {
-    title: "Live like a king in my castle",
-    price: 190,
-    isSuperHost: true,
-    images: ["img/castle1.png", "img/caste2.png"]
+function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    renderGame()
 }
 
-console.log(castle.price)
-console.log(castle.isSuperHost)
+function renderGame() {
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
 
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
+}
+
+
+function newCard() {
+    if (isAlive === true && hasBlackJack === false) {
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        renderGame()
+    }
+}
