f5a915ad9c4d4a8085531d0135610aa57d8f11fc
saibatechnoexponent <saibal@technoexponent.com>
2022-08-09 16:08:22 +0530

    The logical OR operator

--- index.js
+++ index.js
@@ -56,30 +56,17 @@
 // }
 
 
-//logical AND operator:
-let hasCompletedCourse = true
-let givesCertificate = true
+//logical OR operator:
+// Create two boolean variables, likesDocumentaries and likesStartups
+// Use an OR statement (||) to call recommendMovie() if either of those variables are true
 
-if (hasCompletedCourse === true && givesCertificate === true) {
-    generateCertificate()
+let likesDocumentaries = true
+let likesStartups = true
+
+if (likesDocumentaries === true || likesStartups === true) {
+    recommendMovie()
 }
 
-function generateCertificate() {
-    console.log("Generating certificate....")
-}
-
-
-
-let hasSolvedChallenge = false
-let hasHintsLeft = false
-
-// Create an if statement that checks that both variables are false.
-// If so, run the showSolution() function
-
-if (hasSolvedChallenge === false && hasHintsLeft === false) {
-    showSolution()
-}
-
-function showSolution() {
-    console.log("Showing the solution....")
+function recommendMovie() {
+    console.log("Hey, check out this new film we think you will like!")
 }
